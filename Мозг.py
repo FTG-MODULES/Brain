8e0a90eb0f938d71c364795b7a36b98cede3323f
FTG - MODULES <96304146+FTG-MODULES@users.noreply.github.com>
2022-11-06 10:35:33 +0500

    Add files via upload

---
+++ Мозг.py
@@ -0,0 +1,32 @@
+
+from telethon.tl.types import Message
+from .. import loader, utils
+
+@loader.tds
+class МозгMod(loader.Module):
+    """мозг"""
+
+    strings = {"name": "мозг"}
+
+    @loader.command()
+    async def мозгcmd(self, message: Message):
+        """<реплай + текст> - отправляет видео с мозгом по реплаю"""
+        reply = await message.get_reply_message()
+        args = utils.get_args_raw(message)
+        m = "https://t.me/HACKER_PHONE_VIP_CHAT/1492480"
+        if reply:
+            await self.client.send_file(message.chat_id, file=m, caption=f'<i> © {args}</i>', reply_to=reply)
+        else:
+            await message.respond(file=m)
+        if message.out:
+            await message.delete()
+            
+# meta developer: @AstroModulesMini
+# 
+#
+# This module was created by the
+# AstroModules team specifically for this
+# channel.  There is no license and banner
+# here, since it is not really needed
+#
+#                           © AstroModules 
